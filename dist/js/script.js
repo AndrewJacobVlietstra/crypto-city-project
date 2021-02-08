f75ae324abc10ddab737177d51ea34f6aace70a0
Andrew <vlietstra123@hotmail.com>
2021-02-08 11:47:34 -0500

    Modified JS so when nav link is clicked, close the navbar on mobile

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,6 +13,12 @@ $(document).ready(function (){
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks =  document.getElementsByClassName('main-nav-list')[0];
 
+// When clicking menu item, toggle navbarlinks to display as flex items
 toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+})
+
+// When clicking a navbarlink, hide the navbarlinks
+navbarLinks.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 })
