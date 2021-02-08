5bdf1d91a48ec9a2b224f42705bececd4707ac02
Andrew <vlietstra123@hotmail.com>
2021-02-08 11:41:28 -0500

    Implemented responsive navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,17 @@
 $(document).ready(function (){
     $(".carousel").slick({
         autoplay: true,
-        autoplaySpeed: 8000,
+        autoplaySpeed: 6500,
         dots: true
     });
 });
+
+
+
+// For Responsive Navbar
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks =  document.getElementsByClassName('main-nav-list')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+})
